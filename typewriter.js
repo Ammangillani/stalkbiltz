d88b129cf15c0625da67e9b71c09037713521b3d
Amman Gillani <ammangillani4321@icloud.com>
2024-03-27 06:29:03 +0500

    commit

--- typewriter.js
+++ typewriter.js
@@ -1,50 +1,40 @@
-// The typewriter element
-var typeWriterElement = document.getElementById("typewriter");
+var typed;
 
-// The TextArray:
-var textArray = [
-  "Hey, I'm Tim.",
-  "I like JavaScript.",
-  "I Love to Develop.",
-  "I like this Typewriter.",
-];
-
-// function to generate the backspace effect
-function delWriter(text, i, cb) {
-  if (i >= 0) {
-    typeWriterElement.innerHTML = text.substring(0, i--);
-    // generate a random Number to emulate backspace hitting.
-    var rndBack = 10 + Math.random() * 100;
-    setTimeout(function () {
-      delWriter(text, i, cb);
-    }, rndBack);
-  } else if (typeof cb == "function") {
-    cb();
-  }
-}
-
-// function to generate the keyhitting effect
-function typeWriter(text, i, cb) {
-  if (i < text.length + 1) {
-    typeWriterElement.innerHTML = text.substring(0, i++);
-    // generate a random Number to emulate Typing on the Keyboard.
-    var rndTyping = 250 - Math.random() * 100;
-    setTimeout(function () {
-      typeWriter(text, i++, cb);
-    }, rndTyping);
-  } else if (i === text.length + 1) {
-    setTimeout(function () {
-      delWriter(text, i, cb);
-    }, 1000);
-  }
-}
-
-// the main writer function
-function StartWriter(i) {
-  var index = i % textArray.length; // Use modulo to loop through the textArray
-  typeWriter(textArray[index], 0, function () {
-    StartWriter(i + 1);
+function initializeTyped() {
+  typed = new Typed(".auto-type", {
+    strings: ["Code."],
+    typeSpeed: 0,
+    backSpeed: 0,
+    loop: false,
   });
 }
-// start the typewriter
-StartWriter(0);
+
+function initializeFullTyped() {
+  typed = new Typed(".auto-type", {
+    strings: [
+      "Code.",
+      "Learn New Framework.",
+      "Teach New Framework.",
+      "Collaborate.",
+      "Present Live Demo.",
+      "Run live Example.",
+      "Create Bug Report.",
+      "Review Bug Report.",
+      "Get Feedback.",
+    ],
+    typeSpeed: 20,
+    backSpeed: 10,
+    loop: true,
+  });
+}
+
+function checkScreenSize() {
+  if (window.innerWidth < 768) {
+    initializeTyped();
+  } else {
+    initializeFullTyped();
+  }
+}
+
+checkScreenSize();
+window.addEventListener("resize", checkScreenSize);
