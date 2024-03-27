2ff48100c1bb965690d6f0526913b530f9b323e3
Amman Gillani <ammangillani4321@icloud.com>
2024-03-28 02:53:07 +0500

    commit

--- hemberg.js
+++ hemberg.js
@@ -1,13 +1,16 @@
 let close = document.getElementById("close");
 let open = document.getElementById("open");
 let nav = document.querySelector(".nav-content");
+let spanclose = document.getElementById("spanclose");
 
 function openmenu() {
   nav.style.right = "20px";
   open.style.display = "none";
-  close.style.display = "block";
+  spanclose.style.display = "block";
 }
 function closemenu() {
   nav.style.right = "-1000px";
+  open.style.display = "block";
+  spanclose.style.display = "none";
 }
 close.addEventListener("click", closemenu);
